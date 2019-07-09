e9b334ab613bbe389d9fc50acc9ef5760d280ec7
Tapan Behera <tapankumarbehera@gmail.com>
2019-07-09 22:59:09 +0530

    Test checkin

---
+++ ms-school/src/main/java/com/tapan/ms/msschool/MsSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.tapan.ms.msschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSchoolApplication.class, args);
+	}
+
+}
---
+++ ms-school/src/test/java/com/tapan/ms/msschool/MsSchoolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tapan.ms.msschool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsSchoolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
